75ff0bed692df012829d5ca873199a4dcfe2b316
sjalan73 <sjalan73@in.ibm.com>
2019-05-25 22:18:39 +0900

    adding code

---
+++ checkBst.py
@@ -0,0 +1,41 @@
+### A program to check if a binary tree is BST or not
+
+class Node(object):
+    def __init__(self,data):
+        self.left = None 
+        self.right = None
+        self.data = data
+
+
+
+def checkBst(root,
+
+
+
+
+
+root = Node(4)
+root.left = Node(2)
+root.right = Node(5)
+root.left.left = Node(1)
+root.left.right = Node(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
